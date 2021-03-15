fa7d98370f408dbc2da32d298841449a173726e6
geoffreyweal <geoffrey.weal@gmail.com>
2021-03-15 02:16:40 -0700

    update version of Organisms Jupyter Examples

--- Notebooks/Example_of_Au38/RunMinimisation_Au.py
+++ Notebooks/Example_of_Au38/RunMinimisation_Au.py
@@ -16,7 +16,7 @@ Other outputs: Trajectory file.
 import sys
 import time
 from asap3.Internal.BuiltinPotentials import Gupta
-from ase.optimize import BFGS, FIRE
+from ase.optimize import FIRE
 
 def Minimisation_Function(cluster,collection,cluster_name):
 	#######################################################################################
@@ -24,7 +24,13 @@ def Minimisation_Function(cluster,collection,cluster_name):
 	#######################################################################################
 	# Perform the local optimisation method on the cluster.
 	# Parameter sequence: [p, q, a, xi, r0]
-	Gupta_parameters = {'Au': [10.229, 4.0360, 0.2061, 1.7900, 2.884]}
+	#
+	# RGL parameters below from:
+	# Crossover among structural motifs in transition and noble-metal clusters
+	# F. Baletto, R. Ferrando, A. Fortunelli, F. Montalenti and C. Mottet, J. Chem. Phys., 2002, 116, 3856–3863.
+	# https://doi.org/10.1063/1.1448484
+	r0 = 4.07/(2.0**0.5)
+	Gupta_parameters = {'Au': [10.53, 4.30, 0.2197, 1.855, r0]}
 	cluster.set_calculator(Gupta(Gupta_parameters, cutoff=1000, debug=False))
 	dyn = FIRE(cluster,logfile=None)
 	startTime = time.time(); converged = False
